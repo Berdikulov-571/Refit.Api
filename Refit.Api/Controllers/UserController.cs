acaba3d4974707b637574e076b46e53921f37410
berdikulov-571 <mygithub571@gmail.com>
2023-12-11 12:38:02 +0500

    Добавьте файлы проекта.

---
+++ Refit.Api/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Refit.Api.Models;
+
+namespace Refit.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            List<User> users = new List<User>()
+            {
+                new User() { UserId = 1,FirstName = "Sanjarbek",LastName = "Berdikulov"},
+                new User() { UserId = 2,FirstName = "Alisher",LastName = "Almatov"},
+                new User() { UserId = 3,FirstName = "Ahror",LastName = "Ummadjonov"},
+                new User() { UserId = 4,FirstName = "Diyorbek",LastName = "Berdiyev"},
+                new User() { UserId = 5,FirstName = "Sherdor",LastName = "Saydullayev"}
+            };
+
+            return Ok(users);
+        }
+    }
+}
---
+++ Refit.Api/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Refit.Api.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
